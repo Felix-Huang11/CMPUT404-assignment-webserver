f3f004363b1bedffef04b1f69bd87d90b3afde77
Zihao Huang <zihao@192.168.0.105>
2021-01-29 04:03:28 +0800

    pass

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos, Zihao Huang
 # 
@@ -27,12 +28,96 @@ import socketserver
 # try: curl -v -X GET http://127.0.0.1:8080/
 
 
+def respond200(uri):
+
+    root = "./www"
+
+    if (uri[-1] == "/"):
+        root += uri + "index.html"
+    else: 
+        root += uri
+    path = uri.split("/")
+    go_in = go_out = 0
+    for each in path:
+        if each == "..":
+            go_out += 1
+        if each != "..":
+            go_in += 1
+
+    if go_out > go_in:
+        return respond404()
+        
+    try:
+        body = open(root).read()
+    except:
+        return respond404()
+
+    status = "HTTP/1.1 200 OK\r\n"
+    if (uri[-1] == "/"):
+        
+        content_type = "Content-type: text/html;charset=utf-8\r\n"
+    else:
+        content_type = "Content-type: text/"+uri.split("/")[-1].split(".")[-1]+";charset=utf-8\r\n"
+        
+    content_length = "Content-Length: "+ str(len(body))+"\r\n\n"
+
+    response = status + content_type + content_length + body
+
+    return response
+
+def respond301(uri):
+    response = "HTTP/1.1 301 Moved Permanently\r\nLocation: "+uri +"/"+"\r\n\n"
+    return response
+
+def respond404():
+    status = "HTTP/1.1 404 File not found\r\n"
+    content_type = "Content-type: text/html;charset=utf-8\r\n"
+    
+    body = "<html><body><h1>404 PAGE NOT FOUND</h1></body></html>"
+    content_length = "Content-Length: "+ str(len(body))+"\r\n\n"
+
+    response = status + content_type + content_length + body
+    return response
+
+def respond405():
+
+    status = "HTTP/1.1 405 Method Not Allowed\r\nAllow: GET, HEAD\r\n"
+    content_type = "Content-type: text/html;charset=utf-8\r\n"
+    
+    body = "<html><body><h1>405 METHOD NOT ALLOWED</h1></body></html>"
+    content_length = "Content-Length: "+ str(len(body))+"\r\n\n"
+
+    response = status + content_type + content_length + body
+    return response
+
+def getResponse(request):
+    if (request[0] == 'GET'):
+        uri = request[1]
+        path = "www"+uri+"/"
+        if (uri[-1] != "/" and os.path.isdir(path)):
+            return respond301(uri)
+        
+        return respond200(uri)
+
+    else:
+        return respond405()
+
+
 class MyWebServer(socketserver.BaseRequestHandler):
     
+    # def handle(self):
+    #     self.data = self.request.recv(1024).strip()
+    #     print ("Got a request of: %s\n" % self.data)
+    #     self.request.sendall(bytearray("OK",'utf-8'))
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        raw_request = self.data.decode().split('\n')
+        HTTPRequest = raw_request[0]
+
+        request = (HTTPRequest.split()[0], HTTPRequest.split()[1])
+        response = getResponse(request)
+        self.request.sendall(response.encode())
+    
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
